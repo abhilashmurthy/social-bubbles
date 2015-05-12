0de166d398a34f783ec362198a12806fe973c894
Abhilash Murthy <abhilashmurthy92@gmail.com>
2015-05-13 02:07:37 +0800

    Bound data between view, controller, and directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,19 @@
  */
 angular.module('socialBubblesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	$scope.addDate = function() {
+		$scope.d3Data.push({
+			dateText: $scope.newDate.split(',')[0],
+			close: parseInt($scope.newDate.split(',')[1])
+		});
+  	};
+
+  	$scope.d3Data = [
+		{dateText: '4-Apr-12', close: 34},
+		{dateText: '5-Apr-12', close: 45},
+		{dateText: '6-Apr-12', close: 37},
+		{dateText: '7-Apr-12', close: 56},
+		{dateText: '8-Apr-12', close: 50},
+		{dateText: '9-Apr-12', close: 77}
+  	];
   });
--- app/scripts/directives/simplelinechart.js
+++ app/scripts/directives/simplelinechart.js
@@ -10,13 +10,25 @@ angular.module('socialBubblesApp')
   .directive('simpleLineChart', ['d3Service', function(d3Service) {
     return {
       restrict: 'EA',
-      scope: {},
+      scope: {
+        data: '=' //bi-directional data-binding
+      },
       link: function(scope, element, attrs) {
         d3Service.d3().then(function(d3) {
+          if (!attrs) {
+            console.log('oldVals is unused');
+          }
 
+          /**** STATIC VARIABLES, THESE DO NOT GET AFFECTED BY DATA CHANGE ****/
           var margin = {top: 20, right: 20, bottom: 30, left: 50},
             width = 600 - margin.left - margin.right,
             height = 700 - margin.top - margin.bottom;
+
+          var svg = d3.select(element[0]).append('svg')
+           .attr('width', width + margin.left + margin.right)
+           .attr('height', height + margin.top + margin.bottom)
+           .append('g')
+           .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
           var parseDate = d3.time.format('%d-%b-%y').parse;
 
@@ -34,34 +46,6 @@ angular.module('socialBubblesApp')
             .scale(y)
             .orient('left');
 
-          var line = d3.svg.line()
-            .x(function(d) { return x(d.date); })
-            .y(function(d) { return y(d.close); });
-
-          var svg = d3.select(element[0]).append('svg')
-           .attr('width', width + margin.left + margin.right)
-           .attr('height', height + margin.top + margin.bottom)
-           .append('g')
-           .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-          // Hard coded data
-          scope.data = [
-            {date: '4-Apr-12', close: 34},
-            {date: '5-Apr-12', close: 45},
-            {date: '6-Apr-12', close: 37},
-            {date: '7-Apr-12', close: 56},
-            {date: '8-Apr-12', close: 50},
-            {date: '9-Apr-12', close: 77}
-          ];
-
-          scope.data.forEach(function(d) {
-            d.date = parseDate(d.date);
-            d.close = +d.close;
-          });
-
-          x.domain(d3.extent(scope.data, function(d) { return d.date; }));
-          y.domain(d3.extent(scope.data, function(d) { return d.close; }));
-
           svg.append('g')
             .attr('class', 'x axis')
             .attr('transform', 'translate(0,' + height + ')')
@@ -77,10 +61,38 @@ angular.module('socialBubblesApp')
             .style('text-anchor', 'end')
             .text('Price ($)');
 
-          svg.append('path')
-            .datum(scope.data)
-            .attr('class', 'line')
-            .attr('d', line);
+          /**** THIS LISTENS TO NEW DATA FROM THE CONTROLLER ****/
+          scope.$watch('data', function(newVals, oldVals) {
+            if (!oldVals) {
+              console.log('oldVals is unused');
+            }
+            return scope.render(newVals);
+          }, true);
+
+          /*** RE-RENDER ON NEW DATA from scope.$watch ***/
+          scope.render = function(data) {
+            scope.data = data;
+
+            scope.data.forEach(function(d) {
+              d.date = parseDate(d.dateText);
+              d.close = +d.close;
+            });
+
+            x.domain(d3.extent(scope.data, function(d) { return d.date; }));
+            y.domain(d3.extent(scope.data, function(d) { return d.close; }));
+
+            svg.selectAll('.line').remove();
+
+            var line = d3.svg.line()
+              .x(function(d) { return x(d.date); })
+              .y(function(d) { return y(d.close); });
+
+            svg.append('path')
+              .datum(scope.data)
+              .attr('class', 'line')
+              .attr('d', line);
+          };
+
         });
       }};
     }]);
